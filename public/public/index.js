07c34fc065ff7d25e6a029ea91f30ef4292bde9b
SouleyBah21 <souley2183@gmail.com>
2025-04-16 22:51:18 -0400

    Update index.js

--- public/public/index.js
+++ public/public/index.js
@@ -1,19 +1,17 @@
 const facts = [
-  "Did you know? Honey never spoils.",
-  "Did you know? The Eiffel Tower can be 15 cm taller during the summer.",
-  "Did you know? A small child could swim through the veins of a blue whale.",
-  "Did you know? Octopuses have three hearts.",
-  "Did you know? Bananas are berries, but strawberries are not."
+  "The moon is slowly drifting away from Earth.",
+  "Honey never spoils.",
+  "Bananas are berries, but strawberries aren't.",
+  "Octopuses have three hearts.",
+  "A single strand of spaghetti is called a 'spaghetto'."
 ];
 
 addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
+  event.respondWith(handleRequest(event.request));
+});
 
 async function handleRequest(request) {
   const randomFact = facts[Math.floor(Math.random() * facts.length)];
-  return new Response(JSON.stringify({ fact: randomFact }), {
-    headers: { 'Content-Type': 'application/json' },
-  });
+  return new Response(randomFact, { status: 200 });
 }
 
